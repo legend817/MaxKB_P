41f56cb56b314f0532863919c0bc467a816998a5
legend817 <73658427+legend817@users.noreply.github.com>
2025-02-12 12:16:24 +0800

    Update user_serializers.py

--- apps/users/serializers/user_serializers.py
+++ apps/users/serializers/user_serializers.py
@@ -185,9 +185,7 @@ class RegisterSerializer(ApiMixin, serializers.Serializer):
 
         return True
 
-    @valid_license(model=User, count=2,
-                   message=_(
-                       "The community version supports up to 2 users. If you need more users, please contact us (https://fit2cloud.com/)."))
+
     @transaction.atomic
     def save(self, **kwargs):
         m = User(
@@ -772,9 +770,7 @@ class UserManageSerializer(serializers.Serializer):
             if self.data.get('password') != self.data.get('re_password'):
                 raise ExceptionCodeConstants.PASSWORD_NOT_EQ_RE_PASSWORD.value.to_app_api_exception()
 
-    @valid_license(model=User, count=2,
-                   message=_(
-                       'The community version supports up to 2 users. If you need more users, please contact us (https://fit2cloud.com/).'))
+    
     @transaction.atomic
     def save(self, instance, with_valid=True):
         if with_valid:
