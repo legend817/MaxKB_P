b56d8b9cc25a4b8c6691d93369f31ecb7af7768a
legend817 <73658427+legend817@users.noreply.github.com>
2025-02-12 12:11:32 +0800

    Update valid_serializers.py

--- apps/setting/serializers/valid_serializers.py
+++ apps/setting/serializers/valid_serializers.py
@@ -46,10 +46,4 @@ class ValidSerializer(serializers.Serializer):
         model_value = model_message_dict.get(self.data.get('valid_type'))
         xpack_cache = DBModelManage.get_model('xpack_cache')
         is_license_valid = xpack_cache.get('XPACK_LICENSE_IS_VALID', False) if xpack_cache is not None else False
-        if not is_license_valid:
-            if self.data.get('valid_count') != model_value.get('count'):
-                raise AppApiException(400, model_value.get('message'))
-            if QuerySet(
-                    model_value.get('model')).count() >= model_value.get('count'):
-                raise AppApiException(400, model_value.get('message'))
         return True
